3be6cddecb39661fac5d758a45f17ba3b14d51e6
Buzzgrow <artur_pg2011@hotmail.com>
2020-07-19 16:13:55 -0300

    Rudimentary backend

--- script.js
+++ script.js
@@ -6,6 +6,21 @@
     var $btnCE = doc.querySelector('span[data-js="ce"]');
     var $output = doc.querySelector('span[data-js="output"]');
     var $equals = doc.querySelector('span[data-js="equals"]');
+    var $previous = doc.querySelector('span[data-js="previous"]');
+
+    var numbers = [];
+    var op = '';
+    var operations = {
+        '+': function(a, b){return a + b},
+        '-': function(a, b){return a - b},
+        'x': function(a, b){return a * b},
+        '/': function(a, b){return a / b},
+        '%': function(a, b){return a % b},
+    };
+
+    function calculate(numbers, op){
+        return Number(operations[op](numbers[0], numbers[1]).toFixed(2));
+    }
 
     function isNull(){
         return $output.innerHTML === '0' || $output.innerHTML === '';
@@ -13,44 +28,65 @@
 
     $btnNumber.forEach((element) => {
         element.addEventListener('click', function(){
-            isNull() ? $output.innerHTML = element.innerHTML : $output.innerHTML += element.innerHTML;
+            isNull() ? $output.innerHTML = element.innerHTML: $output.innerHTML += element.innerHTML;
         }, false);
     });
 
     $btnOperator.forEach((element) => {
         element.addEventListener('click', function(){
-
-            //some operators have a proper treatment, therefore, the if's are necessary.
-            if(element.innerHTML === 'x²'){
-                $output.innerHTML += ' ' + '^' + ' ';    
-            }else if(element.className === 'operator sqroot'){
-                $output.innerHTML += ' ' + '√' + ' ';    
+            if(element.innerHTML !== '.'){
+                op = element.innerHTML.toLowerCase();
+                numbers.push(+$output.innerHTML);
+                $previous.innerHTML = $output.innerHTML +  ' ' + element.innerHTML.toLowerCase() + ' ';
+                $output.innerHTML = '0';
             }else{
-                $output.innerHTML += ' ' + element.innerHTML.toLowerCase() + ' ';
+                $output.innerHTML += '.';   
             }
-
         }, false);
     });
 
     $btnBackspace.addEventListener('click', function(){
         var output = $output.innerHTML;
 
-        var lastChar = output.endsWith(' ') ? output.slice(output.length-3) : output.slice(output.length-1);
+        if(output.length > 3){
+            var lastChar = output.endsWith(' ') ? output.slice(output.length-3) : output.slice(output.length-1);
+        }else{
+            var lastChar = output.endsWith(' ') ? output.slice(output.length-2) : output.slice(output.length-1);
+        }
 
         $output.innerHTML = output.replace(lastChar, '');
         
         isNull() ? $output.innerHTML = '0' : '';
+
     }, false);
 
     $btnCE.addEventListener('click', function(){
         $output.innerHTML = '0';
+        $previous.innerHTML = '';
+        numbers = [];
+        op = '';
     }, false);
 
     $equals.addEventListener('click', function(){
-        var expression = $output.innerHTML;
-
-        console.log(expression.split(' '));
-
+       
+        if($previous.innerHTML !== ''){
+            $previous.innerHTML += $output.innerHTML + ' = ';
+            
+            if($output.innerHTML === 'π'){
+                if($output.innerHTML !== ''){
+                    numbers.push(Math.PI);
+                    $output.innerHTML = calculate(numbers, op);
+                    numbers = [];
+                }else{
+                    $output.innerHTML = Math.PI.toFixed(2);
+                }
+            }else{
+                numbers.push(+$output.innerHTML);
+                $output.innerHTML = calculate(numbers, op);
+                numbers = [];
+            }
+        }
+        
     }, false);
 
 
