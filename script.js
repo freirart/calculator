0fbae24ddd6267428011c4a7644c89d64828127e
Buzzgrow <artur_pg2011@hotmail.com>
2020-07-21 21:11:10 -0300

    v1.0

--- script.js
+++ script.js
@@ -14,7 +14,7 @@
         '+': function(a, b){return a + b},
         '-': function(a, b){return a - b},
         'x': function(a, b){return a * b},
-        '/': function(a, b){return a / b},
+        '÷': function(a, b){return a / b},
         '%': function(a, b){return a % b},
     };
 
